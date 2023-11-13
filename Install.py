1aa8ded22f34a76b9d13ab410db1fe075917c6c9
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-13 16:08:30 -0700

    Add files via upload

--- Install.py
+++ Install.py
@@ -1,47 +1,55 @@
 import os
-import requests
+import urllib.request
 
-def download_file(url, save_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Downloaded: {url}")
-    else:
-        print(f"Failed to download: {url}")
+# Create directories
+os.makedirs("Hicup/photos", exist_ok=True)
+os.makedirs("Hicup/videos", exist_ok=True)
+os.makedirs("Hicup/My-HTML", exist_ok=True)
+os.chdir("Hicup")
 
-def main():
-    # Create the Hicup directory if it doesn't exist
-    os.makedirs("Hicup", exist_ok=True)
-    os.chdir("Hicup")
+# URLs to download
+urls = [
+    "https://github.com/HttpAnimation/Hicup/raw/main/styles.css",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.php",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.js",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package.json",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package-lock.json",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/index.html",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/apache2.conf",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.sh",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/README.md",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Dockerfile",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.sh",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/000-default.conf"
+]
 
-    # List of URLs to download
-    urls = [
-        "https://github.com/HttpAnimation/Hicup/blob/main/styles.css",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.php",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.js",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package.json",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package-lock.json",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/index.html",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/apache2.conf",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.py",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/README.md",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Dockerfile",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.py",
-        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/000-default.conf"
-    ]
+# Download files
+for url in urls:
+    filename = os.path.basename(url)
+    urllib.request.urlretrieve(url, filename)
 
-    # Download files
-    for url in urls:
-        file_name = url.split("/")[-1]
-        download_file(url, file_name)
+# Change permissions for the Run.sh and Build.sh scripts
+os.chmod("Run.sh", 0o755)
+os.chmod("Build.sh", 0o755)
 
-    # Create photos and videos directories
-    os.system("mkdir photos")
-    os.system("mkdir videos")
+# Change back to the main directory
+os.chdir("..")
 
-    # Install Express using npm
-    os.system("npm install express")
+# Create My-HTML directory and download files
+os.makedirs("My-HTML", exist_ok=True)
+os.chdir("My-HTML")
 
-if __name__ == "__main__":
-    main()
+html_urls = [
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/My-HTML/index.html",
+    "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/My-HTML/styles.css"
+]
+
+for url in html_urls:
+    filename = os.path.basename(url)
+    urllib.request.urlretrieve(url, filename)
+
+# Change back to the main directory
+os.chdir("..")
+
+# Install npm package
+os.system("npm install express")
--- Install.sh
+++ Install.sh
@@ -1,5 +1,8 @@
 mkdir Hicup
 cd Hicup
+mkdir photos
+mkdir videos
+mkdir My-HTML
 wget https://github.com/HttpAnimation/Hicup/blob/main/styles.css
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.php
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.js
@@ -12,9 +15,8 @@ wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/README.md
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Dockerfile
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.sh && chmod +x Build.sh
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/000-default.conf
-mkdir photos
-mkdir videos
-mkdir My-HTML
 cd My-HTML
 wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/My-HTML/index.html
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/My-HTML/styles.css
+cd ../
 npm install express
