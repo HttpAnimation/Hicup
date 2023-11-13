631e91023bed6256352f8ef3a4363c27ff57faad
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-12 22:06:23 -0700

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,45 @@
+import os
+import requests
+
+def download_file(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print(f"Downloaded: {url}")
+    else:
+        print(f"Failed to download: {url}")
+
+def main():
+    # Create the Hicup directory if it doesn't exist
+    os.makedirs("Hicup", exist_ok=True)
+    os.chdir("Hicup")
+
+    # List of URLs to download
+    urls = [
+        "https://github.com/HttpAnimation/Hicup/blob/main/styles.css",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.php",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.js",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package.json",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package-lock.json",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/index.html",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/apache2.conf",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.sh",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.py",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/README.md",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Dockerfile",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.sh",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.py",
+        "https://raw.githubusercontent.com/HttpAnimation/Hicup/main/000-default.conf"
+    ]
+
+    # Download files
+    for url in urls:
+        file_name = url.split("/")[-1]
+        download_file(url, file_name)
+
+    # Install Express using npm
+    os.system("npm install express")
+
+if __name__ == "__main__":
+    main()
--- Install.sh
+++ Install.sh
@@ -1,3 +1,17 @@
 mkdir Hicup
 cd Hicup
+wget https://github.com/HttpAnimation/Hicup/blob/main/styles.css
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.php
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/server.js
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package.json
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/package-lock.json
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/index.html
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/apache2.conf
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.sh
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Run.py
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/README.md
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Dockerfile
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.sh
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/Build.py
+wget https://raw.githubusercontent.com/HttpAnimation/Hicup/main/000-default.conf
 npm install express
