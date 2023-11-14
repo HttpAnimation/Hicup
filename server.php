0544fb869dc2fbc3746a909b4ec956f99cdb2a50
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-14 10:28:18 -0700

    adds use Firefox line to server.php

--- server.php
+++ server.php
@@ -7,10 +7,10 @@ foreach ($files as $file) {
         $filePath = "$folder/$file";
         if ($_GET['type'] === 'videos') {
             // For videos, generate a video tag
-            echo "<video controls width='640' height='360'><source src='$filePath' type='video/mp4'>Your browser does not support the video tag.</video>";
+            echo "<video controls width='640' height='360'><source src='$filePath' type='video/mp4'>Your browser does not support the video tag use Firefox.</video>";
         } else if ($_GET['type'] === 'music') {
             // For music, generate an audio tag
-            echo "<audio controls><source src='$filePath' type='audio/mp3'>Your browser does not support the audio tag.</audio>";
+            echo "<audio controls><source src='$filePath' type='audio/mp3'>Your browser does not support the audio tag use Firefox.</audio>";
         } else {
             // For photos, generate an img tag
             echo "<img src='$filePath' style='max-width: 400px; margin: 10px;'>";
