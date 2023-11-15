d3446f86800620bb325b7328606c7fb97aa17bc2
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-15 11:08:32 -0800

    moves all old testing windows code to a folder called old

--- Build.py
+++
@@ -1,3 +0,0 @@
-# This file is for all systems to build the docker for Linux and MacOS tho use the Build.sh script
-import os
-os.system("docker build -t hicup .")
---
+++ Old/Fast-Dev-Build.py
@@ -0,0 +1,2 @@
+import os
+os.system("docker build -t hicup .")
